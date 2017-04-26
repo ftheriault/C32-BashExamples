76ee39a9d3b2830da0d843495c373882718f4ae4
Frederic <fred.theriault@gmail.com>
2017-04-26 12:20:47 -0400

    Added 2 small bash exercises.

---
+++ Exercice 1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# La ligne suivante permet de vérifier si un mot existe dans le dictionnaire
+# Il faut suffixer cet URL avec le mot entré par l'uasger (ex: word=chat)
+#wget -qO - http://dictionary.objectif8.com/exists.php?word=
